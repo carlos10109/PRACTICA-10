756c8a453d324367d34047caa292f12795f767fc
carlos10109 <47263414+carlos10109@users.noreply.github.com>
2020-03-06 11:28:39 -0600

    Add files via upload

---
+++ practica10/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>practica10</title>
+ <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+<body>
+<?php
+
+//cometario 1 linea
+/*comentario
+multilinea*/
+
+echo "<h1>HOLA MUNDO</h1>";
+
+?>
+
+
+    
+</body>
+</html>
+
+
+
